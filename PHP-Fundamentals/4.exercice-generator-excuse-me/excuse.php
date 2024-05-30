c998107acf22295f4d4d3c6ab61c2c4c815cfa30
Masy Jordan <masyjordan@gmail.com>
2024-05-30 09:45:02 +0200

    finish ex

--- PHP-Fundamentals/4.exercice-generator-excuse-me/excuse.php
+++ PHP-Fundamentals/4.exercice-generator-excuse-me/excuse.php
@@ -1,35 +1,75 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Excuse</title>
-</head>
-<body>
-<form action="excuse.php" method="POST">
-        <label for="childName">Name of the child:</label><br>
-        <input type="text" id="childName" name="childName" required><br><br> childName         <!-- childName -->
-        
-        <label>Gender of the child:</label><br>
-        <input type="radio" id="girl" name="childGender" value="girl" required><!-- childGender  -->
-        <label for="girl">Girl</label><br>
-        <input type="radio" id="boy" name="childGender" value="boy" required><!-- childGender  -->
-        <label for="boy">Boy</label><br><br>
-        
-        <label for="teacherName">Name of the teacher:</label><br>
-        <input type="text" id="teacherName" name="teacherName" required><br><br><!-- teacherName -->
+<html>
 
-        <label>Reason for the absence:</label><br>
-        <input type="radio" id="illness" name="absenceReason" value="illness" required><!-- absenceReason -->
+<head>
+    <title>Excuse Generator</title>
+</head>
+
+<body>
+    <h1>Excuse Note Generator</h1>
+    <form method="post" action="">
+        <label for="child_name">Name of the child:</label>
+        <input type="text" id="child_name" name="child_name" required><br><br>
+
+        <label for="gender">Gender of the child:</label>
+        <input type="radio" id="boy" name="gender" value="boy" required>
+        <label for="boy">Boy</label>
+        <input type="radio" id="girl" name="gender" value="girl" required>
+        <label for="girl">Girl</label><br><br>
+
+        <label for="teacher_name">Name of the teacher:</label>
+        <input type="text" id="teacher_name" name="teacher_name" required><br><br>
+
+        <label for="reason">Reason for absence:</label><br>
+        <input type="radio" id="illness" name="reason" value="illness" required>
         <label for="illness">Illness</label><br>
-        <input type="radio" id="deathPet" name="absenceReason" value="deathPet" required><!-- absenceReason -->
-        <label for="deathPet">Death of the pet (or a family member)</label><br>
-        <input type="radio" id="extraCurricular" name="absenceReason" value="extraCurricular" required><!-- absenceReason -->
-        <label for="extraCurricular">Significant extra-curricular activity</label><br>
-        <input type="radio" id="other" name="absenceReason" value="other" required><!-- absenceReason -->
-        <label for="other">Any other excuse of your choice</label><br><br>
-        
-        <input type="submit" value="Submit">
+        <input type="radio" id="death_pet" name="reason" value="death_pet" required>
+        <label for="death_pet">Death of a pet</label><br>
+        <input type="radio" id="death_family" name="reason" value="death_family" required>
+        <label for="death_family">Death of a family member</label><br>
+        <input type="radio" id="activity" name="reason" value="activity" required>
+        <label for="activity">Significant extra-curricular activity</label><br>
+        <input type="radio" id="other" name="reason" value="other" required>
+        <label for="other">Other</label><br><br>
+
+        <button type="submit" name="generate">Generate Excuse Note</button>
     </form>
+
+    <?php
+    if(isset($_POST['child_name']) && isset($_POST['teacher_name']) && isset($_POST['reason']) && isset($_POST['gender'])){
+        $child_name = $_POST['child_name'];
+        $gender = $_POST['gender'];
+        $teacher_name = $_POST['teacher_name'];
+        $reason = $_POST['reason'];
+    
+        $pronoun = $gender == 'boy' ? 'He' : 'She';
+        $note = "";
+
+        switch ($reason) {
+            case 'illness':
+                $note = "$child_name was unable to attend school due to illness. $pronoun is now recovering and will be back in school shortly.";
+                break;
+            case 'death_pet':
+                $note = "$child_name was absent from school due to the death of a beloved pet. $pronoun needed some time to cope with the loss.";
+                break;
+            case 'death_family':
+                $note = "$child_name was unable to attend school due to the death of a family member. $pronoun needed time to be with the family during this difficult time.";
+                break;
+            case 'activity':
+                $note = "$child_name was involved in a significant extra-curricular activity and could not attend school. $pronoun will make up for the missed work.";
+                break;
+            case 'other':
+                $note = "$child_name was unable to attend school for personal reasons. Please feel free to contact me for more details.";
+                break;
+        }
+
+        echo "<h2>Copy and Paste This Excuse Note</h2>";
+        echo "<p>Dear $teacher_name,</p>";
+        echo "<p>$note</p>";
+        echo "<p>Thank you for your understanding.</p>";
+        echo "<p>Sincerely,<br>Parent/Guardian</p>";
+    }
+    ?>
 </body>
+
 </html>
