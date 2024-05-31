c8f40b67de40b36efa87b8fb53b90abcb90646d5
MJordanBecode <masyjordan@gmail.com>
2024-05-31 14:36:39 +0200

    function

--- PHP-Fundamentals/8.function/functions.php
+++ PHP-Fundamentals/8.function/functions.php
@@ -11,6 +11,9 @@
     <form action="functions.php" method="post">
         <label for="name">Nom:</label>
         <input type="text" id="name" name="name" required>
+        <br>
+        <label for="name">Chiffre :</label>
+        <input type="text" id="number" name="number" required>
         <br><br>
         <input type="submit" value="Envoyer">
     </form>
@@ -39,9 +42,35 @@ function Capitalizes($name){ //fonctionne :)
 function dateTime(){
 
     $date_now = date('Y-m-d H:i:s');
+
+    return $date_now;
 }
+echo("il est ".dateTime());
+
 // echo("Le nom changé = ".Capitalizes($name));
 
+function sum($a, $b){
+
+    $sum = $a + $b;
+
+    return $sum;
+}
+
+echo('<br>'.'resultat ='.sum(5,4));
+/* Fonction à retravailler */
+if (isset($_POST['number'])) {
+    $number = $_POST['number'];}
+function checkNumber($number) {
+   
+        
+        // Vérifier si le numéro est un entier
+        if (filter_var($number, FILTER_VALIDATE_INT) === false) {
+            echo "Isn't a number, try again";
+        } else {
+            echo "It's a valid number";
+        }
+    
+}
 
 ?>
 </body>
